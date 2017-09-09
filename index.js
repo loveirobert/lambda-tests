c7c2edb9d767eb19584f1cb9c40c16fe6038b0e5
loveirobert <loveirobert@gmail.com>
2017-09-09 07:46:42 +0200

    lambda function

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.handler = (event, context) => {
+  context.succeed("Hello Lambda World!");
+};
+
+// Fuller, Matthew. AWS Lambda: A Guide to Serverless Microservices (Kindle Locations 168-169). UNKNOWN. Kindle Edition. 
